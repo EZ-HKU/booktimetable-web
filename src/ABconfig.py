aa566e7c6aff4a22cb485054eeba80f5f8ca28de
adlsdztony <zzl0712@outlook.com>
2024-04-25 09:31:56 +0800

    Update Single Study Room (3 sessions) Room name in ABconfig.py

--- src/ABconfig.py
+++ src/ABconfig.py
@@ -8,7 +8,7 @@ TIME_LIST =  {
     # generate time list for single study room from 00:00-00:30, 00:30-01:00, ..., 23:30-24:00
     "Study Room": [f'{i//2:>0{2}}{"00" if i%2==0 else "30"}{i//2+i%2:>0{2}}{"00" if i%2!=0 else "30"}' for i in range(47)],
     "Concept and Creation Room": ["08301300", "13001800", "18002200"],
-    "Single Study Room (3 sessions)": ["08301300", "13001800", "18002200"],
+    "Single Study Room (3 sessions) Room": ["08301300", "13001800", "18002200"],
     }
 
 DEBUG = os.environ.get('BOOKING_DEBUG').lower() == 'true'
@@ -19,7 +19,7 @@ def DISCUSSION_ROOM(room):
     return int(room.split(' ')[-1]) + 125
 
 FACILITY = {'Discussion Room': DISCUSSION_ROOM}
-FTYPE = {'Discussion Room': '21', 'Single Study Room (3 sessions)': '31'}
+FTYPE = {'Discussion Room': '21', 'Single Study Room (3 sessions) Room': '31'}
 LIBRARY = {'Main Library': '3'}
 FLOOR = {'Level 3': '3', '4/F': '4'}
 
@@ -39,5 +39,5 @@ LENLIMIT ={
     'Discussion Room': 2,
     'Study Room': 4,
     'Concept and Creation Room': 1,
-    'Single Study Room (3 sessions)': 1,
+    'Single Study Room (3 sessions) Room': 1,
 }
